651e7cb3ecd922c4b10b3bb0e5e16fdfe9ec7557
Hunk_Man <sergejarenkov@gmail.com>
2024-05-18 21:39:03 +0300

    Исправил ошибку с языком и перегрузил операторы

--- Hospital/Func.cpp
+++ Hospital/Func.cpp
@@ -4,16 +4,45 @@
 #include "Division.h"
 using namespace std;
 
-string Dis::getTime() {
-	return time;
+istream& operator >>(istream& in, Patient& pat) {
+	cout << "Enter name:";
+	in >> pat._fio;
+	cout << "Enter sex:";
+	in >> pat._gender;
+	cout << "Enter age:";
+	in >> pat._age;
+	cout << "Enter diagnosis:";
+	in >> pat._diagnosis;
+	//cout << "Enter time of arrival:";
+	//in >> pat._time;
+	cout << "Enter doctor`s name:";
+	in >> pat._doctor;
+	cout << "Enter status:";
+	in >> pat._status;
+	return in;
 }
 
-void Dis::setTime(string _time) {
-	time = _time;
+ostream& operator <<(ostream& out, Patient& pat) {
+	out << " ФИО: " << pat._fio << " Пол: " << pat._gender << " Возраст: " << pat._age << " Диагноз: " << pat._diagnosis << " ФИО врача: " << pat._doctor << " Статус: " << pat._status << endl;
 }
 
-string Doc::getDisease() {
-	return disease;
+istream& operator >>(istream& in, Division& div) {
+	cout << "Введите кол-во мест";
+	in >> div.places;
+}
+
+ostream& operator <<(ostream& out, Division& div) {
+	out << "Число мест:" << div.places << endl; 
+	out << "Доктора:";
+	for (Doctor& doctor : div.doctors)
+		out << doctor << endl;
+	out << "Пациенты:";
+	for (Patient& patient : div.patients)
+		out << patient << endl;
+}
+
+int Disease::getRecoveryTime() {
+	return recoveryTime;
 }
 
 string Doc::getPatient() {
