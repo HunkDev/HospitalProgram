264c03cbeae417a44fcb846f71029b2677931b5b
Hunk_Man <sergejarenkov@gmail.com>
2024-05-21 20:33:55 +0300

    Добавил работу со временем(молю создателя плюсов, чтобы оно заработало)

--- Hospital/Main.cpp
+++ Hospital/Main.cpp
@@ -6,25 +6,45 @@
 #include "Func.cpp"
 #include "database.cpp"
 #include "division_database.h"
+#include "time.cpp"
 
 using namespace std;
 
-int main() {
-	cout << "1-Division, 2-Doctor, 3-Patient" << endl;
-	int key;
-	if (!(cin >> key).good()) {
-		cout << "Error" << endl;
-		return 0;
+int Timing(int key) {
+	time_t cur_time = time(nullptr);
+	tm ct_cur;
+	localtime_s(&ct_cur, &cur_time);
+	int cur_day = ct_cur.tm_yday;
+	time_t stored_time = read_time();
+	tm ct_store;
+	localtime_s(&ct_store, &stored_time);
+	int store_day = ct_store.tm_yday;
+	if ((cur_time - stored_time) != 0) {
+		key = 4;
+		write_time(cur_time);
 	}
+	else
+		write_time(cur_time);
+	return key;
+}
+
+int main() {
+	cout << "1-Division, 2-Doctor, 3-Patient 4-advance time" << endl;
+	int key;
+	Timing(key);
+	if(key != 4)
+		if (!(cin >> key).good()) {
+			cout << "Error" << endl;
+			return 0;
+		}
 	switch (key) {
 	case 1:
 	{
 		bool w = true;
-		int n;
 		vector<Division> divisions;
 		while (w) {
 			int key_div;
-			cout << "1-Create, 2-read, 3-search, 4-edit, 5-delete, 6-edit, 7-exit" << endl;
+			cout << "1-Create, 2-read, 3-search, 4-add, 5-delete, 6-exit" << endl;
 			cin >> key_div;
 			if (!(cin >> key_div).good()) {
 				cout << "Error" << endl;
@@ -55,9 +75,6 @@ int main() {
 				division_db::delete_division(divisions);
 				break;
 			case 6:
-				//wtf
-				break;
-			case 7:
 				w = false;
 				break;
 			}
@@ -66,11 +83,10 @@ int main() {
 	}
 	case 2:
 		bool w = true;
-		int n;
 		//Doctor* doctors;
 		while (w) {
 			int key_doc;
-			cout << "1-Create, 2-read, 3-search, 4-edit, 5-delete, 6-edit, 7-exit" << endl;
+			cout << "1-Create, 2-read, 3-search, 4-add, 5-delete, 6-exit" << endl;
 			cin >> key_doc;
 			if (!(cin >> key_doc).good()) {
 				cout << "Error" << endl;
@@ -95,26 +111,23 @@ int main() {
 				//delete_doc(&doctors, &n);
 				break;
 			case 6:
-				//hz
-				break;
-			case 7:
 				w = false;
 				break;
 			}
 		}
 		break;
 	case 3:
-    bool w = true;
+	{
+		bool w = true;
 		vector<Patient> temp;
-		int n;
 		while (w) {
-      int key_pat;
-		  cout << "1-Create, 2-Save, 3-read, 4-edit, 5-delete, 6-edit, 7-exit" << endl;
-		  cin >> key_pat;
-		  if (!key_pat) {
-			  cout << "Error" << endl;
-			  return;
-		  }
+			int key_pat;
+			cout << "1-Create, 2-Save, 3-read, 4-add, 5-delete, 6-exit" << endl;
+			cin >> key_pat;
+			if (!key_pat) {
+				cout << "Error" << endl;
+				return;
+			}
 			switch (key_pat) {
 			case 1:
 				patient_db::create_patients(temp);
@@ -140,12 +153,17 @@ int main() {
 				patient_db::delete_patient(temp);
 				break;
 			case 6:
-
-				break;
-			case 7:
 				w = false;
 				break;
 			}
+		}
+		}
+	case 4:
+		vector<Patient> patients;
+		patient_db::read_patients(patients);
+		int s = (patients.size()-1);
+		for (int i = 0; i < s; i++) {
+			patients[i].advance_day();
 		}
 	}
 }	
