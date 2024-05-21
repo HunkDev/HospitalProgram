b04a239bcb795f935fbb0445a07fb2b082e6a889
Hunk_Man <sergejarenkov@gmail.com>
2024-05-21 21:57:49 +0300

    test

--- Hospital/Main.cpp
+++ Hospital/Main.cpp
@@ -6,6 +6,7 @@
 #include "Func.cpp"
 #include "database.cpp"
 #include "division_database.h"
+#include "doctors_db.cpp"
 #include "time.cpp"
 
 using namespace std;
@@ -39,6 +40,7 @@ int main() {
 		}
 	switch (key) {
 	case 1:
+	{
 		bool w = true;
 		vector<Division> divisions;
 		while (w) {
@@ -50,8 +52,10 @@ int main() {
 				return 0;
 			}
 			switch (key_div) {
-				//Ìåòîäû áä äëÿ îòäåëåíèÿ
 			case 1:
+			{
+				vector<Division> arr;
+				division_db::read_divisions(arr);
 				if (!arr.empty()) {
 					int var;
 					cout << "Divisions database will delete. Do you really want this?" << endl << "1 = yes, \t 2 = no" << endl;
@@ -59,40 +63,49 @@ int main() {
 					if (var == 2)
 						break;
 					if (var == 1) {
-						division_db::create_divisions(&divisions, &n);
+						division_db::create_divisions(divisions);
 						break;
 					}
 					else {
 						cout << "ERROR #12-43840812837531251425" << endl;
-						break
+						break;
 					}
 				}
 				else {
-					division_db::create_divisions(&divisions, &n);
+					division_db::create_divisions(divisions);
 					break;
 				}
+			}
 			case 2:
-				division_db::read_divisions(&divisions, &n);
+				division_db::read_divisions(divisions);
 				break;
 			case 3:
-				division_db::search_division(&divisions, &n);
+			{
+				string str;
+				if (!(cin >> str).good()) {
+					cout << "Error" << endl;
+					return;
+				}
+				division_db::search_division(divisions, str);
 				break;
+			}
 			case 4:
 				//no edit, add
-				division_db::add_division(&divisions, &n);
+				division_db::add_division(divisions);
 				break;
 			case 5:
-				division_db::delete_division(&divisions, &n);
+				division_db::delete_division(divisions);
 				break;
 			case 6:
-					break;
+				break;
 				w = false;
 				break;
 			}
 		}
 		break;
-
+	}
 	case 2:
+	{
 		bool w = true;
 		//int n;
 		//Doctor* doctors;
@@ -110,43 +123,42 @@ int main() {
 				return 0;
 			}
 			switch (key_doc) {
-				//Ìåòîäû áä äëÿ âðà÷à
 			case 1:
-				if (!arr.empty()) {
+				if (!db == NULL) {
 					int var;
 					cout << "Doctors database will delete. Do you really want this?" << endl << "1 = yes, \t 2 = no" << endl;
 					cin >> var;
 					if (var == 2)
 						break;
 					if (var == 1) {
-						doctor_bd::create_doc(db);
+						doctor_db::create_doc(db);
 						break;
 					}
 					else {
 						cout << "ERROR #12-43840812837531251426" << endl;
-						break
+						break;
 					}
 				}
 				else {
-					doctor_bd::create_doc(db);
+					doctor_db::create_doc(db);
 					break;
 				}
 			case 2:
 				//read_doc(&doctors, &n);
-				doctor_bd::read_doc(db);
+				doctor_db::read_doc();
 				break;
 			case 3:
 				//search_doc(&doctors, &n);
-				doctor_bd::search_doc(db);
+				doctor_db::search_doc(db);
 				break;
 			case 4:
 				//add, no edit
 				//add_doc(&doctors, &n);
-				doctor_bd::add_doc(db);
+				doctor_db::add_doc(db);
 				break;
 			case 5:
 				//delete_doc(&doctors, &n);
-				doctor_bd::delete_doc(db);
+				doctor_db::delete_doc(db);
 				break;
 			case 6:
 				w = false;
@@ -155,6 +167,7 @@ int main() {
 		}
 		db.close();
 		break;
+	}
 	case 3:
 	{
 		bool w = true;
@@ -198,11 +211,13 @@ int main() {
 		}
 		}
 	case 4:
+	{
 		vector<Patient> patients;
 		patient_db::read_patients(patients);
-		int s = (patients.size()-1);
+		int s = (patients.size() - 1);
 		for (int i = 0; i < s; i++) {
 			patients[i].advance_day();
 		}
+	}
 	}
 }	
